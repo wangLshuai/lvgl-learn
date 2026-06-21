1a4147687c68d6fecafa25f20110b6786e231759
Shuai Wang <wang.lin.shuai@qq.com>
2026-06-21 11:05:42 +0800

    feat: lvgl 使用外部 sram 做内存池。增大内存池尺寸为 100 KB

--- Middlewares/lvgl/lv_conf.h
+++ Middlewares/lvgl/lv_conf.h
@@ -69,13 +69,13 @@
 
 #if LV_USE_STDLIB_MALLOC == LV_STDLIB_BUILTIN
     /** Size of memory available for `lv_malloc()` in bytes (>= 2kB) */
-    #define LV_MEM_SIZE (26 * 1024U)          /**< [bytes] */
+    #define LV_MEM_SIZE (100 * 1024U)          /**< [bytes] */
 
     /** Size of the memory expand for `lv_malloc()` in bytes */
     #define LV_MEM_POOL_EXPAND_SIZE 0
 
     /** Set an address for the memory pool instead of allocating it as a normal array. Can be in external SRAM too. */
-    #define LV_MEM_ADR 0     /**< 0: unused*/
+    #define LV_MEM_ADR 0x68000000     /**< 0: unused*/
     /* Instead of an address give a memory allocator that will be called to get a memory pool for LVGL. E.g. my_malloc */
     #if LV_MEM_ADR == 0
         #undef LV_MEM_POOL_INCLUDE
