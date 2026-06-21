b9be3a81a44e0337fe150bc7f2f45fa3e4775315
Shuai Wang <wang.lin.shuai@qq.com>
2026-06-21 10:58:00 +0800

    fix: 在 ioc 文件中配置 freertos heapsize，fsmc time
    重新生成代码,代码逻辑并未改变

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -357,13 +357,13 @@ static void MX_FSMC_Init(void)
   hsram4.Init.AsynchronousWait = FSMC_ASYNCHRONOUS_WAIT_DISABLE;
   hsram4.Init.WriteBurst = FSMC_WRITE_BURST_DISABLE;
   /* Timing */
-  Timing.AddressSetupTime = 2;
-  Timing.AddressHoldTime = 0;
-  Timing.DataSetupTime = 5;
-  Timing.BusTurnAroundDuration = 0;
-  Timing.CLKDivision = 0;
-  Timing.DataLatency = 0;
-  Timing.AccessMode = FSMC_ACCESS_MODE_B;
+  Timing.AddressSetupTime = 0;
+  Timing.AddressHoldTime = 15;
+  Timing.DataSetupTime = 2;
+  Timing.BusTurnAroundDuration = 15;
+  Timing.CLKDivision = 16;
+  Timing.DataLatency = 17;
+  Timing.AccessMode = FSMC_ACCESS_MODE_A;
   /* ExtTiming */
 
   if (HAL_SRAM_Init(&hsram4, &Timing, NULL) != HAL_OK)
